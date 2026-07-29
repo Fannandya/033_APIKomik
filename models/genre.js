5b851ab0e5d7fb04e345390cfb731219034e81a2
Fannandya Sutan <sfannandya@gmail.com>
2026-07-29 09:54:24 +0700

    inisialisasi tabel genre

---
+++ models/genre.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const Genre = sequelize.define("Genre", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    nama_genre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  });
+  return Genre;
+};
